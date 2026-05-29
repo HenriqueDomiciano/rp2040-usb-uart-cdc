3d84a2a562168946ee026fde4629e05d77ba723c
Henrique Domiciano <henriquedomiciano@yahoo.com>
2026-05-28 21:58:34 -0300

    [FIX] Fix the Uart bridge Task so it works as expected

--- src/bridge/uart.rs
+++ src/bridge/uart.rs
@@ -1,48 +1,53 @@
-use embassy_futures::join::join;
+use embassy_futures::{
+    join::join,
+    select::{select, Either},
+};
+use embassy_time::{Duration, Timer};
 use embedded_io_async::{Read, Write};
 
 use crate::bridge::channels::{BridgeChannels, Packet};
 
-
-pub async fn uart_task<R, W>(
-    mut rx: R,
-    mut tx: W,
-    channels: &'static BridgeChannels,
-)
+pub async fn uart_task<R, W>(mut rx: R, mut tx: W, channels: &'static BridgeChannels)
 where
     R: Read + 'static,
     W: Write + 'static,
 {
     let uart_rx = async {
         let mut data = [0u8; 64];
+
         loop {
-            // Read first byte (blocking wait)
             match rx.read(&mut data[0..1]).await {
                 Ok(1) => {}
                 _ => continue,
             }
+
             let mut n = 1;
-            // Try to read more bytes without waiting
+
             while n < 64 {
-                match rx.read(&mut data[n..n+1]).await {
-                    Ok(1) => n += 1,
-                    _ => break,
-                }
-                // If no more bytes immediately available, stop accumulating
-                // Use a short timeout to batch bytes arriving close together
-                if n < 64 {
-                    match embassy_time::with_timeout(
-                        embassy_time::Duration::from_micros(500),
-                        rx.read(&mut data[n..n+1])
-                    ).await {
-                        Ok(Ok(1)) => n += 1,
-                        _ => break,
+                let next_byte_or_timeout = select(
+                    rx.read(&mut data[n..n + 1]),
+                    Timer::after(Duration::from_millis(2)), 
+                )
+                .await;
+
+                match next_byte_or_timeout {
+                    Either::First(Ok(1)) => {
+                        n += 1;
+                    }
+                    _ => {
+                        break;
                     }
                 }
             }
             let mut packet_data = [0u8; 64];
             packet_data[..n].copy_from_slice(&data[..n]);
-            channels.uart_to_usb.send(Packet { data: packet_data, len: n }).await;
+            channels
+                .uart_to_usb
+                .send(Packet {
+                    data: packet_data,
+                    len: n,
+                })
+                .await;
         }
     };
 
--- src/tasks/led.rs
+++ src/tasks/led.rs
@@ -1,14 +1,11 @@
 use core::f32;
 
-
-
 use embassy_time::{Duration, Ticker};
 
-use libm::pow;
 use crate::drivers::led::SingleWs2812;
+use libm::pow;
 
 use {defmt_rtt as _, panic_probe as _};
-
 
 fn breathe(t: f32) -> u8 {
     let value = pow(((libm::sinf(t) + 1.0) * 0.5) as f64, 2.0) * 60.0;
@@ -17,30 +14,16 @@ fn breathe(t: f32) -> u8 {
 
 #[embassy_executor::task]
 pub async fn ws2812_task(mut ws2812: SingleWs2812<'static>) {
-    let mut ticker = Ticker::every(Duration::from_millis(30));
+    let mut ticker = Ticker::every(Duration::from_millis(40));
     let mut time: f32 = 0.0;
-    let mut counter : u8 = 0; 
-    loop {
-            defmt::info!("LED tick");
-            let sin_value = breathe(time);
-            match counter%6 {
-               0 => ws2812.write(0, sin_value, 0),
-               1 => ws2812.write(sin_value/2, sin_value, 0),
-               2 => ws2812.write(sin_value/2, sin_value, sin_value/2),
-               3 => ws2812.write(sin_value, sin_value/2, 0),
-               4 => ws2812.write(sin_value, sin_value/2, sin_value/2),
-               5 => ws2812.write(sin_value/2, sin_value/2, sin_value),
-               _ => ws2812.write(0, 0, sin_value), 
-                
-            }
-            ws2812.write(0,sin_value,0);
-            time += 0.1; 
-            if time > f32::consts::TAU
-            {
-                counter += 1; 
-                time = 0.0;
-            }   
-            ticker.next().await;
+    cortex_m::interrupt::free(|_| loop {
+        defmt::info!("LED tick");
+        let sin_value = breathe(time);
+        ws2812.write(0, sin_value, 0);
+        time += 0.1;
+        if time > f32::consts::TAU {
+            time = 0.0;
         }
+    });
+    ticker.next().await;
 }
-
